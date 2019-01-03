0f21bfd24e96a2abe1daefe8a7813c31aa1e258e
JohnnyT <ubergeek3141@gmail.com>
2019-01-03 15:22:49 -0700

    Renders JSON for the preview

--- app/controllers/riddler_admin/steps_controller.rb
+++ app/controllers/riddler_admin/steps_controller.rb
@@ -37,6 +37,8 @@ module RiddlerAdmin
       @use_case = ::Riddler::UseCases::PreviewStep.new definition,
         params: params.to_unsafe_h,
         headers: request_headers
+
+      render json: @use_case.process
     end
 
     # POST /steps
