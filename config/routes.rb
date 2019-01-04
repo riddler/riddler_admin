ef4e6ee35b6d7c1aa764c681c2439bc9d775774e
Cody Stringham <cs.negativ@gmail.com>
2019-01-04 11:22:58 -0700

    adds preview enabled toggle to show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RiddlerAdmin::Engine.routes.draw do
   resources :steps do
     post "preview", on: :member
     get "preview", on: :member
+    get "toggle", on: :member
   end
 
   resources :elements do
