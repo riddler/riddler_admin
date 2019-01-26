ae012a86d783b48f65caf60ff494543a3219381c
JohnnyT <ubergeek3141@gmail.com>
2019-01-26 09:46:04 -0700

    Allows for a blank user_can_approve_block

--- app/helpers/riddler_admin/application_helper.rb
+++ app/helpers/riddler_admin/application_helper.rb
@@ -1,11 +1,16 @@
 module RiddlerAdmin
   module ApplicationHelper
     def current_user
-      controller.send ::RiddlerAdmin.config.current_user_method
+      controller.send config.current_user_method
     end
 
     def user_can_approve?
-      ::RiddlerAdmin.config.user_can_approve_block.call current_user
+      return true if config.user_can_approve_block.nil?
+      config.user_can_approve_block.call current_user
+    end
+
+    def config
+      ::RiddlerAdmin.config
     end
   end
 end
